f97778bd7a1ca43d0666f8c8702d535d6e6a990a
Julia Romualdo <75337060+romualdojulia@users.noreply.github.com>
2022-05-10 18:03:57 -0300

    Add files via upload

---
+++ Aula04/ex01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+	$numero = 9;
+	$cont = 0;
+	
+    for($cont=1; $cont<=10; $cont++){
+    	echo "{$numero} X {$cont} = " . ($numero * $cont) ."<br>";
+    }
+?>
+
+</body>
+</html>
